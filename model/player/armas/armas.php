4e88d25fd5d3a69ee8cd15fea812651372c32729
CristianG456 <cristiancronos123@gmail.com>
2025-10-07 12:27:52 -0500

    construccion de la pestaña de armas junto a su css

--- model/player/armas/armas.php
+++ model/player/armas/armas.php
@@ -5,7 +5,8 @@ $db = new Database();
 $con = $db->conectar();
 
 
-$sql = $con->prepare("SELECT * FROM armas");
+
+$sql = $con->prepare("SELECT * FROM armas where armas.id_arma order by armas.id_arma ASC");
 $sql->execute();
 $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -18,8 +19,9 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Armas</title>
-     <link rel="stylesheet" href="../../../controller/css/lobby.css">
+     <link rel="stylesheet" href="../../../controller/css/armas.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet"> 
+    
 </head>
 <body>
      <!-- Video de fondo -->
@@ -29,35 +31,40 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
 
     <!-- Contenido centrado arriba -->
     <div class="contenedor">
-        <h1>Armamento</h1>
         
-
+        <!-- volver  a lobby-->
+        <a href="../player.php" class="btn-volver">Volver</a>
+        
         <div>
-            <h2 style="margin-top: 40px;">Armas</h2>
+            <h2 class="page-title-armamento">Armamento</h2>
 
             <!-- armas y fondo oscuro -->
 
             <div class="armas-wrapper">
                 <div class="armas-container">
-                    <div class="arma">
-                        <img src="../../../controller/img/puño.jpg" alt="Puño">
-                        <p>Puño</p>
-                    </div>
+                    <?php
+                    
+                    if (!empty($resultado)) {
+                        foreach ($resultado as $arma) {
+                            
+                            $img = $arma['img_arma'];
+                            $img = trim($img, '"');
+                            $img = trim($img, '"');
+                            
+                            $imgBasename = basename($img);
+                            $imgPath = '../../../controller/img/' . $imgBasename;
+                            $nombre = htmlspecialchars($arma['nomb_arma']);
 
-                    <div class="arma">
-                        <img src="../../../controller/img/cuchillo.png" alt="Cuchillo" style="width: 65px; height: auto;">
-                        <p>Cuchillo</p>
-                    </div>
-
-                    <div class="arma">
-                        <img src="../../../controller/img/pistola.png" alt="Pistola">
-                        <p>Pistola</p>
-                    </div>
-
-                    <div class="arma">
-                        <img src="../../../controller/img/SUBFUSIL.png" alt="M249" style="width: 150px; height: auto;">
-                        <p>Subfusil</p>
-                    </div>
+                            echo "<div class=\"arma\">";
+                            echo "<img src=\"{$imgPath}\" alt=\"{$nombre}\">";
+                            echo "<p>{$nombre}</p>";
+                            
+                            echo "</div>";
+                        }
+                    } else {
+                        echo "<p>No hay armas registradas.</p>";
+                    }
+                    ?>
                 </div>
             </div>
         </div>
@@ -66,22 +73,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
             
 
             
-            <div class="armas-wrapper">
-                <div class="armas-container">
-                    <div class="arma">
-                        <img src="../../../controller/img/fusilasalto.png" alt="M9">
-                        <p>Fuisil de Asalto</p>
-                    </div>
-
-                    <div class="arma">
-                        <img src="../../../controller/img/escopeta.png" alt="Bailiff 410">
-                        <p>Escopeta</p>
-                    </div>
-
-                    <div class="arma">
-                        <img src="../../../controller/img/franco.png" alt="Pistola SD">
-                        <p>Rifle de Presicion</p>
-                    </div>
+            
 
                 </div>
             </div>
